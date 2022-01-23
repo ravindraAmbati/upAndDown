921ac32f8bf3c0f379329971c315fb2623c92cfd
Ravindra Reddy Ambati <ravindra.reddy.ambati@outlook.in>
2022-01-23 13:36:39 +0530

    spring init

---
+++ src/main/java/play/dice/upAndDown/UpAndDownApplication.java
@@ -0,0 +1,13 @@
+package play.dice.upAndDown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpAndDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpAndDownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/play/dice/upAndDown/UpAndDownApplicationTests.java
@@ -0,0 +1,13 @@
+package play.dice.upAndDown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpAndDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
